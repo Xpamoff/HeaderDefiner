be3349775463252a73b157aa7dc2d0ddc5bb3752
Sheepwithdick <kazancev789@mail.ru>
2022-04-15 18:17:10 +0400

    A couple of renamings

--- main.py
+++ main.py
@@ -1,67 +1,21 @@
-import json, urllib.request
-import time
+from flask import Flask, request, render_template
+import wordstat_reports
+import header_definer
 
-url = 'https://api-sandbox.direct.yandex.ru/v4/json/'
-token = 'AQAAAAATz2ALAAfGKxA_tjoNVk5ivNjTlx1yQ1o'
-
-
-def create_report(token, phrase):
-    data = {
-        'method': 'CreateNewWordstatReport',
-        'token': token,
-        'locale': 'ru',
-        'param': {
-            "Phrases": [
-                phrase
-            ],
-            "GeoID": [
-                0
-            ]
-        }
-    }
-
-    jdata = json.dumps(data, ensure_ascii=False).encode('utf8')
-    response = urllib.request.urlopen(url, jdata)
-    num = response.read().decode('utf8')
-
-    id = ''
-    for i in range(8, len(num) - 1):
-        id = id + (str(num[i]))
-    print('Запрос сформирован')
-    return id
+app = Flask(__name__)
 
 
-def get_report(token, report_id):
-    data = {
-        'token': token,
-        'method': 'GetWordstatReport',
-        'param': report_id
-    }
-    jdata = json.dumps(data, ensure_ascii=False).encode('utf8')
-    response = urllib.request.urlopen(url, jdata)
-    report_str = response.read().decode('utf8')
-    report_dict = json.loads(report_str)
-    try:
-        report = report_dict['data'][0]['SearchedAlso']
-        return report
-    except KeyError:
-        print(report_dict)
-        return -1
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
-def phrase_choose(report):
-    sorted_report = sorted(report, key=lambda d: d['Shows'], reverse=True)
-    print(sorted_report)
-    count = 0
-    for i in sorted_report:
-        print(i['Phrase'] + ": " + str(i['Shows']))
-        count += 1
-        if count == 5:
-            break
+@app.route('/get_data', methods=['GET', 'POST'])
+def get_data():
+    data = request.form.get('article')
+
+    return render_template('get_data.html', data=data)
 
 
-report_id = create_report(token, "путин умер")
-time.sleep(6)
-new_report = get_report(token, report_id)
-if new_report != -1:
-    phrase_choose(new_report)
+if __name__ == '__main__':
+    app.run()
---
+++ wordstat_reports.py
@@ -0,0 +1,68 @@
+import json, urllib.request
+import time
+
+url = 'https://api-sandbox.direct.yandex.ru/v4/json/'
+token = 'AQAAAAATz2ALAAfGKxA_tjoNVk5ivNjTlx1yQ1o'
+
+
+def create_report(token, phrase):
+    data = {
+        'method': 'CreateNewWordstatReport',
+        'token': token,
+        'locale': 'ru',
+        'param': {
+            "Phrases": [
+                phrase
+            ],
+            "GeoID": [
+                0
+            ]
+        }
+    }
+
+    jdata = json.dumps(data, ensure_ascii=False).encode('utf8')
+    response = urllib.request.urlopen(url, jdata)
+    num = response.read().decode('utf8')
+
+    id = ''
+    for i in range(8, len(num) - 1):
+        id = id + (str(num[i]))
+    print('Запрос сформирован')
+    return id
+
+
+def get_report(token, report_id):
+    data = {
+        'token': token,
+        'method': 'GetWordstatReport',
+        'param': report_id
+    }
+    jdata = json.dumps(data, ensure_ascii=False).encode('utf8')
+    response = urllib.request.urlopen(url, jdata)
+    report_str = response.read().decode('utf8')
+    report_dict = json.loads(report_str)
+    try:
+        report = report_dict['data'][0]['SearchedAlso']
+        return report
+    except KeyError:
+        print(report_dict)
+        return -1
+
+
+def phrase_choose(report):
+    sorted_report = sorted(report, key=lambda d: d['Shows'], reverse=True)
+    # print(sorted_report)
+    count = 0
+    for i in sorted_report:
+        print(i['Phrase'] + ": " + str(i['Shows']))
+        count += 1
+        if count == 5:
+            break
+
+
+if __name__ == "__main__":
+    report_id = create_report(token, "Медведчук")
+    time.sleep(6)
+    new_report = get_report(token, report_id)
+    if new_report != -1:
+        phrase_choose(new_report)
